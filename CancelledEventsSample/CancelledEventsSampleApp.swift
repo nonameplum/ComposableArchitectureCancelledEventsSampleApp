1fa9790ea9be4eb33d3d9e48186da65150e34d20
Łukasz Śliwiński <sliwinski.lukas@gmail.com>
2025-09-04 17:11:43 +0200

    Initial Commit

---
+++ CancelledEventsSample/CancelledEventsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CancelledEventsSampleApp.swift
+//  CancelledEventsSample
+//
+//  Created by Łukasz Śliwiński on 04/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CancelledEventsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CancelledEventsSampleTests/CancelledEventsSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  CancelledEventsSampleTests.swift
+//  CancelledEventsSampleTests
+//
+//  Created by Łukasz Śliwiński on 04/09/2025.
+//
+
+import Testing
+@testable import CancelledEventsSample
+
+struct CancelledEventsSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
